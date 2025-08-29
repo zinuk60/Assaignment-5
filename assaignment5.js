bdea7eef8aa222b560d5cb995a18748e55affdf1
zinuk60 <zinukdebnath6010@gmail.com>
2025-08-29 18:20:31 +0600

    added functionality for call-button and clear button

--- assaignment5.js
+++ assaignment5.js
@@ -1,3 +1,5 @@
+//  code for heart icon
+
 const accessLoveIcon=document.getElementsByClassName("fa-regular")
 const accessHeartCount=document.getElementById("heart-count")
 let heartCount=parseInt(accessHeartCount.innerText)
@@ -8,3 +10,53 @@ for(let i=0;i<accessLoveIcon.length;i++){
          accessHeartCount.innerText=heartCount
     })
 }
+
+
+// code for call button
+
+const accessCallButton=document.getElementsByClassName("call-button")
+const accessCoinCount=document.getElementById("coin-count")
+let coinCount=parseInt(accessCoinCount.innerText)
+
+for(let i=0;i<accessCallButton.length;i++){
+    
+    const modifyI=i+1
+
+ 
+     const accessHistoryItem=document.getElementById("history-item-"+modifyI)
+
+     const accessHistoryTitle=document.getElementById("history-title-"+modifyI)
+     const accessHistoryNumber=document.getElementById("history-number-"+modifyI)
+     const time=document.getElementById("time-"+modifyI)
+     const accessTitle=document.getElementById("title-"+modifyI).innerText
+    const accessNumber=document.getElementById("number-"+modifyI).innerText
+ 
+
+    accessCallButton[i].addEventListener('click',function(){
+       
+        if(coinCount<=0){
+            alert("you have no coin")
+            return
+        }
+        alert("calling "+accessTitle+ " "+accessNumber)
+         coinCount-=20
+        accessCoinCount.innerText=coinCount
+         accessHistoryTitle.innerText=accessTitle
+         accessHistoryNumber.innerText=accessNumber
+         const now=new Date()
+         let hour=now.getHours()
+         let minit=now.getMinutes()
+         let second=now.getSeconds()
+         time.innerText=hour+":"+minit+":"+second
+
+        accessHistoryItem.style.display="flex"
+       
+    })
+}
+
+const accessClear=document.getElementById("clear-button")
+accessClear.addEventListener("click",function(){
+    for(let i=1;i<10;i++){
+        document.getElementById("history-item-"+i).style.display="none"
+    }
+})
